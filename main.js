395f838a809d34d4d44d299c4680344d3d49fade
De Guzman-9S-AY2526 <deguzman.etc@obmontessori.edu.ph>
2026-04-03 10:34:58 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function calculateArea(rad) {
-var rad = Number(document.getElementById("rad").value);
-var area = Math.PI * (rad ** 2);
-document.getElementById("area").value = area;
+function calculateArea() {
+    var rad = Number(document.getElementById("rad").value);
+    var area = Math.PI * Math.pow(rad, 2);
+    document.getElementById("area").value = area.toFixed(2);
 }
